53aeb6bb40993126b59859546f267cdf54787d65
Joshua Tuscan <hello@joshuatuscan.com>
2015-07-13 19:20:43 -0700

    social buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,28 @@
 
 'use strict';
 
+var modalOperator = {
+    open: function() {
+        $('.modal').fadeIn(200);
+    },
+    close: function() {
+        $('.modal').fadeOut(200);
+    }
+};
+
 $(document).ready(function() {
-    console.log('ready!');
+
+    $('.snapchat a').click(function(e){
+        e.preventDefault();
+
+        modalOperator.open();
+    });
+
+    $('.modal').click(function(e){
+        e.preventDefault();
+
+        modalOperator.close();
+    });
 });
 
 
