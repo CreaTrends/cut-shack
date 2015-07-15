88e2953acd070e01934fcec71457e4630e459585
Joshua Tuscan <hello@joshuatuscan.com>
2015-07-15 14:07:20 -0700

    running game

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,5 @@
 /*eslint-env node, jquery */
+/*global Modernizr:true*/
 
 'use strict';
 
@@ -12,11 +13,47 @@ if (Modernizr.touch) {
 var modalOperator = {
     open: function() {
         $('.modal').fadeIn(200);
+        $('body').addClass('lock');
     },
     close: function() {
         $('.modal').fadeOut(200);
+        $('body').removeClass('lock');
+    },
+    openEmail: function() {
+        $('.signup-modal').fadeIn(200);
+        $('body').addClass('lock');
+    },
+    closeEmail: function() {
+        $('.signup-modal').fadeOut(200);
+        $('body').removeClass('lock');
     }
 };
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
+
 
 $(document).ready(function() {
     if (isTouch === true) {
@@ -29,10 +66,17 @@ $(document).ready(function() {
         modalOperator.open();
     });
 
-    $('.modal').click(function(e){
+    $('.modal, .signup-modal').click(function(e){
         e.preventDefault();
 
-        modalOperator.close();
+        var me = $(this);
+
+        if (me.hasClass('modal')) {
+            modalOperator.close();
+        } else {
+            modalOperator.closeEmail();
+        }
+        
     });
 });
 
